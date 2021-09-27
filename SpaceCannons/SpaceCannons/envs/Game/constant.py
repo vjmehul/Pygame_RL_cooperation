e2f6fdb410694b67cfadcc43163266bdd4bf0707
Mehul <Mehuljan26@gmail.com>
2021-09-27 09:57:51 +0200

    27-9

---
+++ SpaceCannons/SpaceCannons/envs/Game/constant.py
@@ -0,0 +1,19 @@
+
+DISPLAY_SIZE = (950,700)
+P1_pos_x = 630
+P1_pos_y = -10
+
+P2_pos_x = 250
+P2_pos_y = -10
+
+PLAYER_HEALTH=30
+miniboss_health=40
+mini_health=4
+coop_factor=0.5
+
+game_difficulty_metric = 1
+bullet_penalty = 0.1
+
+
+miniboss_evade_penalty = -2
+mini_evade_penalty = -1
